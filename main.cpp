1e9099e3edc957f336e2013780e99fc5d10e9ec2
killanaca <killanaca@gmail.com>
2012-10-10 18:30:58 +0200

    initial mpi skeleton

--- main.cpp
+++ main.cpp
@@ -1,36 +1,63 @@
 #include <stdio.h>
 #include "fftutils.h"
 #include <iostream>
+#include "mpi.h"
 
 using namespace std;
 
 int main(int argc, char **argv){
 
+  MPI_Init(NULL,NULL);
 
-	//read from configuration file
-	const char* const fName = (argc == 2) ? argv[1] : 0;
+  //get the number of processes
+  int world_size=0;
+  MPI_Comm_size(MPI_COMM_WORLD,&world_size);
 
-	if (fName) {
-         if (!readConfig(fName)) {
-              cout << "Error in config file abort" << endl;  
-              return 0;
-         }
-         
-    } else {
-         cout << "Config file not specified. Executing with default config" << endl;
-    }
+  //get the rank
+  int rankid=0;
+  MPI_Comm_rank(MPI_COMM_WORLD,&rankid);
+	// Get the name of the processor
+  char processor_name[MPI_MAX_PROCESSOR_NAME];
+  int name_len;
+  MPI_Get_processor_name(processor_name, &name_len);
 
-    //print configuration file
-    cout << "BLOCK_SIZE = " << blockSize << endl;
-    cout << "PRINT_RESULT " << print << endl;
-    cout << "xRange = " << xRange << endl;
-    cout << "yRange = " << yRange << endl;
-    cout << "zRange = " << zRange << endl;
+  if(rank==0){
+    printf("Hello world from processor %s, rank %d out of %d processors. THE MASTER\n", processor_name, rankid, world_size);
 
-    //print selected algorithm
-    if (fftAlgo==2){
-    	cout << "FFT algorithm = COOLEY_TUKEY" << endl;
+    //read from configuration file
+  	const char* const fName = (argc == 2) ? argv[1] : 0;
+
+  	if (fName) {
+           if (!readConfig(fName)) {
+                cout << "Error in config file abort" << endl;  
+                return 0;
+           }
+           
+      } else {
+           cout << "Config file not specified. Executing with default config" << endl;
+      }
+
+      //print configuration file
+      cout << "BLOCK_SIZE = " << blockSize << endl;
+      cout << "PRINT_RESULT " << print << endl;
+      cout << "xRange = " << xRange << endl;
+      cout << "yRange = " << yRange << endl;
+      cout << "zRange = " << zRange << endl;
+
+      //print selected algorithm
+      if (fftAlgo==2){
+      	cout << "FFT algorithm = COOLEY_TUKEY" << endl;
+      }else{
+      	cout << "Unknow FFT algorithm" << endl;
+      }
     }else{
-    	cout << "Unknow FFT algorithm" << endl;
+      printf("Hello world from processor %s, rank %d out of %d processors. SLAVE\n", processor_name, rankid, world_size);
+
+
+
     }
+
+
+
+    MPI_Finalize();
 }
