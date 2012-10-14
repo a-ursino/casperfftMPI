675c44bd56813c3649740c662f78ae68bd468205
killanaca <killanaca>
2012-10-15 00:01:13 +0200

    from 2 to 6 process, minimal mpi_scatter

--- main.cpp
+++ main.cpp
@@ -59,9 +59,39 @@ int main(int argc, char **argv){
     }else{
       printf("Hello world from processor %s, rank %d out of %d processors. SLAVE\n", processor_name, rankid, world_size);
 
-
-
     }
+
+    //All
+
+    float *rand_nums, sub_rand_nums;
+    int num_proc=6;
+    int num_elements_per_proc=2
+    if (rankid==0)
+    {
+      rand_nums= (float)malloc(sizeof(float)*num_proc*num_elements_per_proc)
+    }
+    for (int i = 0; i < num_elements_per_proc*num_proc; ++i)
+    {
+      rand_nums[i]=2.5;
+    }
+    sub_rand_nums=(float)malloc(sizeof(float)*num_elements_per_proc);
+    //MPI_Scatter(void* send_data, int send_count, MPI_Datatype send_datatype, void* recv_data, int recv_count, MPI_Datatype recv_datatype, int root, MPI_Comm communicator)
+    MPI_Scatter(rand_nums, num_elements_per_proc, MPI_FLOAT, sub_rand_nums, num_elements_per_proc, MPI_FLOAT, 0, MPI_COMM_WORLD);
+    //MPI_Scatter(rand_nums, num_elements_per_proc, MPI_FLOAT, sub_rand_nums, num_elements_per_proc, MPI_FLOAT, 0, MPI_COMM_WORLD);
+    
+    //cooleyTukeyCpu3DFFT(start, n, sizeOnCPU,hraVec,hiaVec,hrRaVec,hiRaVec,ASPAN,show_result,FFT_type);
+
+    for (int i = 0; i < num_elements_per_proc; ++i)
+    {
+      sub_rand_nums[i]=rankid;
+      printf("Processor %s, rank %d out of %d processors. [%f ]\n",processor_name, rankid, world_size,sub_rand_nums[i] );
+    }
+
+    //MPI_Gather(void* send_data, int send_count, MPI_Datatype send_datatype, void* recv_data, int recv_count, MPI_Datatype recv_datatype, int root, MPI_Comm communicator)
+
+
+
+
 
 
 
